e816928868618142cfc5e8b37522714b1b101dd1
Silvio <silviosisa@gmail.com>
2025-06-10 07:43:23 -0300

    Refactor main.py and update dependencies
    - Simplified main.py by removing unused imports and server initialization code.
    - Added print statement to display the OpenAI API key from environment variables.
    - Updated dependencies in pyproject.toml for langchain and langgraph.
    - Removed math_server.py and weather_server.py as they are no longer needed.
    - Adjusted .gitignore to ensure .env files are properly excluded.

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 import asyncio
-import os
-
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage
-from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import create_react_agent
-from mcp import ClientSession, StdioServerParameters
-from mcp.client.stdio import stdio_client
+import os
+# from langchain_core.messages import HumanMessage
+# from langchain_mcp_adapters.tools import load_mcp_tools
+# from langchain_openai import ChatOpenAI
+# from langgraph.prebuilt import create_react_agent
+# from mcp import ClientSession, StdioServerParameters
+# from mcp.client.stdio import stdio_client
 
 load_dotenv()
+print(os.getenv("OPENAI_API_KEY"))
 
-llm = ChatOpenAI()
-
-stdio_server_params = StdioServerParameters(
-    command="python",
-    args=["/Users/edenmarco/GithubProjects/mcp-crash-course/servers/math_server.py"],
-)
 
 async def main():
-    async with stdio_client(stdio_server_params) as (read,write):    
-        async with ClientSession(read_stream=read, write_stream=write) as session:
-            await session.initialize()
-            print("session initialized")
-            tools = await load_mcp_tools(session)
+    print("Hello from mcp-crash-course!")
 
-
-            agent = create_react_agent(llm,tools)
-
-            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 54 + 2 * 3?")]})
-            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
-

252f735289af83ce04a16924862c024110da7415
Silvio <silviosisa@gmail.com>
2025-06-11 08:02:03 -0300

    Enhance main.py and update dependencies
    - Refactored main.py to include necessary imports for ChatOpenAI and StdioServerParameters.
    - Initialized ChatOpenAI instance and configured StdioServerParameters for server communication.
    - Added isort as a new dependency in pyproject.toml and updated uv.lock accordingly.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import asyncio
-from dotenv import load_dotenv
 import os
-# from langchain_core.messages import HumanMessage
-# from langchain_mcp_adapters.tools import load_mcp_tools
-# from langchain_openai import ChatOpenAI
-# from langgraph.prebuilt import create_react_agent
-# from mcp import ClientSession, StdioServerParameters
-# from mcp.client.stdio import stdio_client
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/admin/code/mcp/section6/mcp-crash-course/servers/math-server.py"],
+)
 
 
 async def main():
